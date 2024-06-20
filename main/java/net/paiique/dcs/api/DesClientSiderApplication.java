242838ab75a3d2a0b86343b53646af4b2c72d9af
paique <bielperrett20@gmail.com>
2024-06-19 23:55:28 -0300

    feat(alzheimer) spring second commit! (Forgot to commit some stuff)

---
+++ main/java/net/paiique/dcs/api/DesClientSiderApplication.java
@@ -0,0 +1,13 @@
+package net.paiique.dcs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesClientSiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesClientSiderApplication.class, args);
+    }
+
+}
---
+++ test/java/net/paiique/dcs/api/DesClientSiderApplicationTests.java
@@ -0,0 +1,13 @@
+package net.paiique.dcs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesClientSiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
